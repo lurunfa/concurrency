539a2fc9879f155ab22e7babe85ac0d711315f9d
lurunfa <lurunfa@163.com>
2018-04-24 22:04:29 +0800

    2018-04-24 项目初始化2

--- src/main/java/com/mmal/concurrency/ServletInitializer.java
+++ src/main/java/com/mmal/concurrency/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.mmal.concurrency;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * @author kevin
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
